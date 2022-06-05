2879d11101e4f51a4a946b2f9b3ce2bf3cc79263
Alver <Alver@DESKTOP-NK6FC4K>
2022-06-05 09:17:22 -0400

    1st name, 2nd name and last name added

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,7 +30,7 @@
         {
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frm1));
-            this.label2 = new System.Windows.Forms.Label();
+            this.lbel1 = new System.Windows.Forms.Label();
             this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
@@ -38,22 +38,24 @@
             this.Mntrp1 = new System.Windows.Forms.MenuStrip();
             this.helpToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
             this.aboutToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.lbel2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.contextMenuStrip1.SuspendLayout();
             this.Mntrp1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // label2
+            // lbel1
             // 
-            this.label2.AutoSize = true;
-            this.label2.BackColor = System.Drawing.Color.Peru;
-            this.label2.Font = new System.Drawing.Font("Times New Roman", 38.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.ForeColor = System.Drawing.SystemColors.Control;
-            this.label2.Location = new System.Drawing.Point(23, 46);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(380, 58);
-            this.label2.TabIndex = 0;
-            this.label2.Text = "Contact Tracing";
-            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lbel1.AutoSize = true;
+            this.lbel1.BackColor = System.Drawing.Color.Peru;
+            this.lbel1.Font = new System.Drawing.Font("Times New Roman", 38.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbel1.ForeColor = System.Drawing.SystemColors.Control;
+            this.lbel1.Location = new System.Drawing.Point(23, 47);
+            this.lbel1.Name = "lbel1";
+            this.lbel1.Size = new System.Drawing.Size(380, 58);
+            this.lbel1.TabIndex = 0;
+            this.lbel1.Text = "Contact Tracing";
+            this.lbel1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // contextMenuStrip1
             // 
@@ -83,12 +85,14 @@
             // 
             // Mntrp1
             // 
-            this.Mntrp1.Dock = System.Windows.Forms.DockStyle.None;
+            this.Mntrp1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.Mntrp1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Visible;
             this.Mntrp1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.helpToolStripMenuItem1});
-            this.Mntrp1.Location = new System.Drawing.Point(9, 9);
+            this.Mntrp1.Location = new System.Drawing.Point(0, 0);
             this.Mntrp1.Name = "Mntrp1";
-            this.Mntrp1.Size = new System.Drawing.Size(52, 24);
+            this.Mntrp1.RenderMode = System.Windows.Forms.ToolStripRenderMode.Professional;
+            this.Mntrp1.Size = new System.Drawing.Size(426, 24);
             this.Mntrp1.TabIndex = 3;
             this.Mntrp1.Text = "menuStrip1";
             // 
@@ -111,12 +115,34 @@
             this.aboutToolStripMenuItem1.Text = "About";
             this.aboutToolStripMenuItem1.Click += new System.EventHandler(this.aboutToolStripMenuItem1_Click);
             // 
+            // lbel2
+            // 
+            this.lbel2.AutoSize = true;
+            this.lbel2.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbel2.ForeColor = System.Drawing.SystemColors.Control;
+            this.lbel2.Location = new System.Drawing.Point(12, 143);
+            this.lbel2.Name = "lbel2";
+            this.lbel2.Size = new System.Drawing.Size(110, 24);
+            this.lbel2.TabIndex = 4;
+            this.lbel2.Text = "First Name";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(16, 170);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(206, 29);
+            this.textBox1.TabIndex = 5;
+            // 
             // frm1
             // 
             this.BackColor = System.Drawing.Color.Sienna;
             this.ClientSize = new System.Drawing.Size(426, 672);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lbel2);
             this.Controls.Add(this.Mntrp1);
-            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lbel1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MainMenuStrip = this.Mntrp1;
             this.Name = "frm1";
@@ -135,7 +161,7 @@
         private System.Windows.Forms.Label lbl2;
         private System.Windows.Forms.TextBox tb1;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lbel1;
         private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
         private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
@@ -143,6 +169,8 @@
         private System.Windows.Forms.MenuStrip Mntrp1;
         private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem1;
         private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem1;
+        private System.Windows.Forms.Label lbel2;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
