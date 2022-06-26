abc6d23182b80a073bd45693ee770f7d221c307b
Alver <Alver@DESKTOP-NK6FC4K>
2022-06-26 08:47:40 -0400

    form3 added for all results to show

--- Form3.Designer.cs
+++ Form3.Designer.cs
@@ -28,34 +28,12 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(78, 36);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(75, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "List of All Data";
-            // 
-            // Form3
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.AutoScroll = true;
-            this.ClientSize = new System.Drawing.Size(552, 563);
-            this.Controls.Add(this.label1);
-            this.Name = "Form3";
+            this.ClientSize = new System.Drawing.Size(800, 450);
             this.Text = "Form3";
-            this.ResumeLayout(false);
-            this.PerformLayout();
-
         }
 
         #endregion
-
-        private System.Windows.Forms.Label label1;
     }
 }
--- Form3.cs
+++ Form3.cs
@@ -7,7 +7,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.IO;
 
 namespace Contact_Tracing
 {
@@ -16,12 +15,6 @@ namespace Contact_Tracing
         public Form3()
         {
             InitializeComponent();
-            var data = Directory.GetFiles(@"C:\Users\Alver\source\repos\Contact-Tracing\Infos");
-            foreach (var file in data)
-            {
-                string allcontent = File.ReadAllText(file);
-                label1.Text = label1.Text + "\n" + allcontent + "\n";
-            }
         }
     }
 }
