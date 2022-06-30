ecb1e765f548ab9fe5bb79d4c8b0473cb7feabf7
Alver <Alver@DESKTOP-NK6FC4K>
2022-06-29 23:11:29 -0400

    controls added on usercontrol

--- user1.Designer.cs
+++ user1.Designer.cs
@@ -35,7 +35,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Name = "user1";
-            this.Size = new System.Drawing.Size(402, 559);
+            this.Size = new System.Drawing.Size(910, 549);
             this.ResumeLayout(false);
 
         }
