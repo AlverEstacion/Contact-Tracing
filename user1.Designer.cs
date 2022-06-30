052a1d543748173f99bf846bc8dac8e537e86bf2
Alver <Alver@DESKTOP-NK6FC4K>
2022-06-30 09:33:33 -0400

    Camera on scanner is now working but not centered

--- user1.Designer.cs
+++ user1.Designer.cs
@@ -28,27 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.components = new System.ComponentModel.Container();
+            this.Scanner = new System.Windows.Forms.PictureBox();
             this.userlbl = new System.Windows.Forms.Label();
             this.StartBTN = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.Devices = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.Scanner)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
-            // pictureBox1
+            // Scanner
             // 
-            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.pictureBox1.Location = new System.Drawing.Point(135, 211);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(453, 431);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
+            this.Scanner.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.Scanner.ImageLocation = "centerscreeen";
+            this.Scanner.Location = new System.Drawing.Point(182, 298);
+            this.Scanner.Name = "Scanner";
+            this.Scanner.Size = new System.Drawing.Size(375, 318);
+            this.Scanner.TabIndex = 0;
+            this.Scanner.TabStop = false;
             // 
             // userlbl
             // 
             this.userlbl.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(103)))), ((int)(((byte)(89)))), ((int)(((byte)(94)))));
             this.userlbl.Font = new System.Drawing.Font("Segoe Script", 39.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.userlbl.ForeColor = System.Drawing.Color.White;
-            this.userlbl.Location = new System.Drawing.Point(43, 17);
+            this.userlbl.Location = new System.Drawing.Point(42, 9);
             this.userlbl.Name = "userlbl";
             this.userlbl.Size = new System.Drawing.Size(641, 177);
             this.userlbl.TabIndex = 40;
@@ -60,33 +67,77 @@
             this.StartBTN.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(103)))), ((int)(((byte)(89)))), ((int)(((byte)(94)))));
             this.StartBTN.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.StartBTN.ForeColor = System.Drawing.Color.White;
-            this.StartBTN.Location = new System.Drawing.Point(296, 652);
+            this.StartBTN.Location = new System.Drawing.Point(296, 661);
             this.StartBTN.Name = "StartBTN";
             this.StartBTN.Size = new System.Drawing.Size(141, 58);
             this.StartBTN.TabIndex = 41;
             this.StartBTN.Text = "Start";
             this.StartBTN.UseVisualStyleBackColor = false;
+            this.StartBTN.Click += new System.EventHandler(this.StartBTN_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(103)))), ((int)(((byte)(89)))), ((int)(((byte)(94)))));
+            this.pictureBox2.Location = new System.Drawing.Point(157, 269);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(430, 377);
+            this.pictureBox2.TabIndex = 42;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Devices
+            // 
+            this.Devices.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.Devices.FormattingEnabled = true;
+            this.Devices.Location = new System.Drawing.Point(262, 232);
+            this.Devices.Name = "Devices";
+            this.Devices.Size = new System.Drawing.Size(201, 21);
+            this.Devices.TabIndex = 43;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Segoe Script", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(103)))), ((int)(((byte)(89)))), ((int)(((byte)(94)))));
+            this.label1.Location = new System.Drawing.Point(294, 192);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(131, 37);
+            this.label1.TabIndex = 44;
+            this.label1.Text = "Camera";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // user1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(238)))), ((int)(((byte)(214)))), ((int)(((byte)(211)))));
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Devices);
             this.Controls.Add(this.StartBTN);
             this.Controls.Add(this.userlbl);
-            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.Scanner);
+            this.Controls.Add(this.pictureBox2);
             this.Name = "user1";
             this.Size = new System.Drawing.Size(731, 722);
             this.Load += new System.EventHandler(this.user1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Scanner)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox Scanner;
         private System.Windows.Forms.Label userlbl;
         private System.Windows.Forms.Button StartBTN;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.ComboBox Devices;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
     }
 }
--- user1.cs
+++ user1.cs
@@ -9,6 +9,10 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using AForge.Video;
 using AForge.Video.DirectShow;
+using AForge;
+using ZXing;
+using ZXing.Aztec;
+using System.IO;
 
 namespace Contact_Tracing
 {
@@ -24,7 +28,41 @@ namespace Contact_Tracing
 
         private void user1_Load(object sender, EventArgs e)
         {
+            filterInfoCollection = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach(FilterInfo filterInfo in filterInfoCollection)
+                Devices.Items.Add(filterInfo.Name);
+            Devices.SelectedIndex = 0;
+        }
 
+        private void StartBTN_Click(object sender, EventArgs e)
+        {
+            captureDevice = new VideoCaptureDevice(filterInfoCollection[Devices.SelectedIndex].MonikerString);
+            captureDevice.NewFrame += CaptureDevice_NewFrame;
+            captureDevice.Start();
+            timer1.Start();
+        }
+
+        private void CaptureDevice_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            Scanner.Image = (Bitmap)eventArgs.Frame.Clone();        
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (Scanner.Image != null)
+            {
+                BarcodeReader barcodeReader = new BarcodeReader();
+                Result result = barcodeReader.Decode((Bitmap)Scanner.Image);
+                if (result != null)
+                {
+                    StreamWriter file = new StreamWriter(@"C:\Users\Alver\source\repos\Contact-Tracing\Infos\QrCodeSubmit.Txt");
+                    file.WriteLine(result.ToString());
+                    timer1.Stop();
+                    if (captureDevice.IsRunning)
+                        captureDevice.Stop();
+                }
+
+            }
         }
     }
 }
